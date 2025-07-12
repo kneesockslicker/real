d91bff83f643b8ea011d8e008d3acd73ff4777b9
fermin <fermin.lim.2023@smu.edu.sg>
2025-07-12 09:32:13 +0900

    changed 142 id.tsx

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -139,7 +139,7 @@ const Post = ({
       try {
         await fetch(
           `https://${
-            process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL
+            process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL
           }/api/views/${pageId}`,
           {
             method: "POST",
